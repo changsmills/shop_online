98aadc9424bef3f46683ffc95a76c13aa9c75def
changsmills <joshuajulius241@gmail.com>
2026-05-10 13:45:09 +0300

    Fix mobile padding and make banner full-width

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -475,11 +475,11 @@ const fetchData = async () => {
 />
     </div>
   )}
-          <div className="content-container-simple" style={{ paddingTop: 0,
-  paddingLeft: isMobile ? 0 : '20px',  // 0 kwenye simu
-  paddingRight: isMobile ? 0 : '20px', // 0 kwenye simu
-  width: '100%',
-  margin: '0 auto'}}>
+     <div className="content-container-simple" style={{ 
+  padding: 0,
+  margin: 0,
+  width: '100%'
+}}>
 
           {search && (
             <div style={{ padding: '15px' }}>
