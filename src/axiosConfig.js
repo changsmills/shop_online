6c8339f7e74e7cb929963cff83e3126b9cd66471
changsmills <joshuajulius241@gmail.com>
2026-07-28 17:30:28 +0300

    Updated requirements.txt to remove mysqlclient and fix django-filter version

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -2,7 +2,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  /* 🔥 BADILISHA HAPA: Kutoka localhost hadi URL ya Render */
+  baseURL: 'https://shop-online-r9z4.onrender.com/api',
 });
 
 // Kila ombi linaongeza token kiotomatiki!
