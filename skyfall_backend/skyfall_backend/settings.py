e358f1671f3822d3f78a345974b1d265cdf35de0
changsmills <joshuajulius241@gmail.com>
2026-07-28 17:49:59 +0300

    Updated requirements.txt to remove mysqlclient and fix django-filter version

--- skyfall_backend/skyfall_backend/settings.py
+++ skyfall_backend/skyfall_backend/settings.py
@@ -125,8 +125,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://localhost:5173',
-    'https://jina-la-frontend-yako.vercel.app', # 🔥 ONGEZA HAPA URL YA LIVE FRONTEND
-
+    'https://shop-online-tan.vercel.app', 
 ]
 
 CORS_ALLOW_CREDENTIALS = True
