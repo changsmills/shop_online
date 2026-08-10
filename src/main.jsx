07b80ac8122cedee1b3e3402e95dfe041b49d14d
changsmills <joshuajulius241@gmail.com>
2026-08-10 23:07:09 +0300

    Weka ujumbe wako hapa

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-//import './App.css';      /* ✅ App.css imepakiwa */
-//import './index.css';    /* ✅ ONGEZA HII! Hapa ndipo variables ziko! */
+import './App.css';      /* ✅ App.css imepakiwa */
 import './i18n';  
 
 ReactDOM.createRoot(document.getElementById("root")).render(
